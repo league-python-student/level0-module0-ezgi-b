49cfee785e509d22d2ecb1c121ef8d9d36ae597e
root <root@root.com>
2021-01-13 00:57:33 +0000

    I finished shapes and colors, tashe, and started logical turtle.

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -8,29 +8,44 @@ if __name__ == '__main__':
     my_turtle = turtle.Turtle()
 
     # Make your turtle's shape 'turtle', .shape('turtle')
-
+    my_turtle.shape('turtle')
     # Set your turtle's speed using .speed(2)
-
+    my_turtle.speed(2)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-
+    my_turtle.color('green')
+    my_turtle.pencolor('blue')
     # Move your turtle forward using .forward(100)
     # TEST    Did your turtle move forward?
+    # my_turtle.forward(100)
 
     # Move your turtle left or right using .left(90) or .right(90)
-
+    # my_turtle.left(90)
+    for i in range(4):
+        my_turtle.forward(100)
+        my_turtle.left(90)
     # Now put the forward and left/right code into a for loop to repeat 4 times.
     # TEST    Did your turtle draw a square?
 
     # Move your turtle to a new place on the screen using .goto(x, y)
     # x=0 and y=0 is the center of the screen
-
+    my_turtle.goto(0, 150)
     # Have your turtle draw a circle using .circle(radius, steps=30)
     # TEST    Did your turtle draw a circle?
-
+    my_turtle.begin_fill()
+    my_turtle.circle(80, steps=30)
+    my_turtle.end_fill()
     # Add color to your shape by adding .begin_fill() before drawing the circle
     # and .end_fill() below
 
     # Draw 3 more shapes with different fill colors!
+    my_turtle.color("orange")
+    my_turtle.begin_fill()
+    my_turtle.circle(50, steps=5)
+    my_turtle.end_fill()
 
+    my_turtle.color("yellow")
+    my_turtle.begin_fill()
+    my_turtle.circle(30, steps=3)
+    my_turtle.end_fill()
     # ===================== DO NOT EDIT THE CODE BELOW ============================
     turtle.done()
